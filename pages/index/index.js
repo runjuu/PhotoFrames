7ff30afb26036f33c7a619919eeed76fecb06a70
Runjuu <mail@ollie.top>
2017-04-20 13:58:51 +0800

    优化操作提示以及载入体验

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,7 +2,6 @@ const params = {};
 let canSelectImage = false;
 
 function drawPhotos(which, showToast) {
-  if (showToast !== false) wx.showToast({ title: '处理中...', icon: 'loading', duration: 10000, mask: true });
   getApp().methods.drawPhotos(
     wx.createCanvasContext(which),
     Object.assign({}, params, which === 'preview' ? { magnification: 1 } : {}),
@@ -14,7 +13,6 @@ Page({
   onReady() {
     const { magnification } = getApp();
     Object.assign(params, { magnification });
-
     canSelectImage = true;
     wx.showToast({ title: '点击，选择图片', icon: 'success', duration: 1000 });
   },
@@ -33,8 +31,8 @@ Page({
   },
 
   previewImage() {
+    wx.showLoading({ title:'处理中...', mask: true });
     canSelectImage = false;
-
     const { canvasToTempFilePath, previewImage } = getApp().methods;
 
     drawPhotos('export');
@@ -43,11 +41,12 @@ Page({
         previewImage({ current: path });
         canSelectImage = true;
       });
+    wx.hideLoading();
   },
 
   zoom({ detail: { value: zoom } }) {
     Object.assign(params, { zoom });
-    if (params.image) drawPhotos('preview', false);
+    if (params.image) drawPhotos('preview');
   },
 
 });
