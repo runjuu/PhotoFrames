dc2b4efeba41f4426cc240045e70e8575490d4ef
Runjuu <mail@ollie.top>
2017-04-20 19:53:23 +0800

    修复安卓导出图片边框透明的问题

--- methods/drawPhotos.js
+++ methods/drawPhotos.js
@@ -1,4 +1,5 @@
 const systemInfo = wx.getSystemInfoSync();
+const { screenWidth } = systemInfo;
 
 function drawPhotos(ctx, { image, magnification, zoom = 100 }) {
   const { filterImage } = getApp().utils;
@@ -7,19 +8,15 @@ function drawPhotos(ctx, { image, magnification, zoom = 100 }) {
   const scale = zoom / 100;
 
 
-  const left = image.width < image.height ? ((systemInfo.screenWidth - imageInfos.width) / 2) * magnification : 0;
-  const top = image.width > image.height ? ((systemInfo.screenWidth - imageInfos.height) / 2) * magnification : 0;
+  const left = image.width < image.height ? ((screenWidth - imageInfos.width) / 2) * magnification : 0;
+  const top = image.width > image.height ? ((screenWidth - imageInfos.height) / 2) * magnification : 0;
   const width = imageInfos.width * magnification * scale;
   const height = imageInfos.height * magnification * scale;
   const widthDiff = (imageInfos.width * magnification - width) / 2;
   const heightDiff = (imageInfos.height * magnification - height) / 2;
 
-  console.log(
-    left + widthDiff,
-    top + heightDiff,
-    width,
-    height,
-  );
+  ctx.setFillStyle('#fff');
+  ctx.fillRect(0, 0, screenWidth * magnification, screenWidth * magnification);
 
   ctx.drawImage(
     image.path,
